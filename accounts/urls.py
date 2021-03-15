aaddbfb179c839cdcef7755653675865e72c0ef5
ohwani <ohwani7@gmail.com>
2021-03-15 19:36:03 +0900

    [DRF] compare apiview

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,19 @@ from . import views
 urlpatterns = [
     path('', views.accountsOverview, name="account-overview"),
     path('user-list/', views.userList, name="user-list"),
-    path('user-list/<int:pk>/', views.userDetail, name="user-detail"),
+    # path('user-list/<int:pk>/', views.userDetail, name="user-detail"),
     path('user-create/', views.userCreate, name="user-create"),
+    # path('user-update/<int:pk>/', views.userUpdate, name="user-update"),
+    # path('user-delete/<int:pk>/', views.userDelete, name="user-delete"),
+    path('userdetail/<int:pk>/', views.userDetail, name="user-Detail")
 ]
+
+
+# from django.urls import path, include
+# from . import views
+
+# urlpatterns = [
+#     # FBV
+#     path('user/', views.UserListAPIView.as_view()),
+#     path('user/<int:pk>/',views.UserDetailAPIView.as_view()),
+# ]
--- accounts/views.py
+++ accounts/views.py
@@ -28,13 +28,6 @@ def userList(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
-def userDetail(request, pk):
-    user = User.objects.get(id=pk)
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
-
-
 @api_view(['POST'])
 def userCreate(request):
     serializer = UserSerializer(data=request.data)
@@ -43,3 +36,89 @@ def userCreate(request):
         serializer.save()
 
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def userDetail(request, pk):
+    if request.method == 'GET':
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+
+    elif request.method == 'PATCH':
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(instance=user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    else:
+        user = User.objects.get(id=pk)
+        user.delete()
+        return Response(serializer.data, status=204)
+
+
+# @api_view(['GET'])
+# def userDetail(request, pk):
+#     user = User.objects.get(id=pk)
+#     serializer = UserSerializer(user, many=False)
+#     return Response(serializer.data)
+
+# @api_view(['PATCH'])
+# def userUpdate(request, pk):
+#     user = User.objects.get(id=pk)
+
+#     serializer = UserSerializer(instance=user, data=request.data)
+
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['DELETE'])
+# def userDelete(request):
+#     user = User.objects.get(id=pk)
+#     user.delete()
+
+#     return Response(serializer.data)
+
+
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from .models import User
+# from .serializers import UserSerializer
+
+
+# class UserListAPIView(APIView):
+#     def get(self, request):
+#         users = User.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#     def post(self, request):
+#         serializer = UserSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+
+#         return Response(serializer.data)
+
+# class UserDetailAPIView(APIView):
+#     def get_object(self, pk):
+#         return get_object_or_404(User, pk=pk)
+
+#     def get(self, request, pk, format=None):
+#         user = self.get_object(pk)
+#         serializer = USerSerializer(user)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         user = self.get_object(pk)
+#         serializer = UserSerializer(user, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         user = self.get_object(pk)
+#         user.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
